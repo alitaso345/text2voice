9132e0dc2b6157589dc1ada322973fb948d65aab
alitaso345 <alice.maru345@gmail.com>
2014-07-21 15:03:24 +0900

    Add method param

--- lib/text2voice.rb
+++ lib/text2voice.rb
@@ -3,26 +3,30 @@ require "text2voice/version"
 module Text2voice
   def initialize(api_key)
     @api_key = api_key
+    @speaker = "show"
+    @pitch = 100.to_s
+    @volume = 100.to_s
   end
 
   def speaker(speaker_name)
-
+    @speaker = speaker_name
   end
 
   def emotion(emotion: nil, level: nil)
-
+    @emotion = emotion
+    @emotion_level = level
   end
 
   def pitch(param)
-
+    @pitch = param.to_s
   end
 
   def volulme(param)
-
+    @volume = param.to_s
   end
 
   def speak(text)
-
+    @text = text
   end
 
   def save(wav)
