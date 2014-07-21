7573e75e073421bff782e03bddfdad26a13f9abf
alitaso345 <alice.maru345@gmail.com>
2014-07-21 13:56:10 +0900

    Add method to create request

--- lib/text2voice.rb
+++ lib/text2voice.rb
@@ -1,5 +1,39 @@
 require "text2voice/version"
 
 module Text2voice
-  # Your code goes here...
+  def initialize(api_key)
+    @api_key = api_key
+  end
+
+  def speaker(speaker_name)
+
+  end
+
+  def emotion(emotion: nil, level: nil)
+
+  end
+
+  def pitch(param)
+
+  end
+
+  def volulme(param)
+
+  end
+
+  def speak(text)
+
+  end
+
+  def save(wav)
+
+  end
+
+  def play(wav)
+
+  end
+
+  def create_request(text, speaker, emotion, emotion_level, pitch, speed, volume)
+
+  end
 end
